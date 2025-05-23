be416a4502c28cb02079002fa9871a35943cc4a2
Dariusz Gralak <dariusz.gralak@xelix.com>
2025-05-23 17:54:08 +0100

    Update sidebar: add tooltips, reorder icons, and apply gradient background

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,51 +1,61 @@
 import Link from "next/link"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
-  faArrowRightArrowLeft, 
-  faFileLines, 
-  faIdCard, 
-  faChartBar, 
-  faFileAlt, 
-  faGear 
+  faArrowRightArrowLeft,
+  faFileInvoice,
+  faFile,
+  faCheck,
+  faAddressBook,
+  faInbox,
+  faChartLine,
+  faGear
 } from '@fortawesome/free-solid-svg-icons'
 
 export default function Sidebar() {
   return (
-    <div className="w-16 flex flex-col bg-[#0f0629] text-white">
+    <div className="w-16 flex flex-col bg-[linear-gradient(rgb(11,11,69)_0%,rgb(59,15,115)_52.08%,rgb(33,8,64)_100%)] text-white">
       <div className="h-16 flex items-center justify-center">
         <Link href="/" className="flex items-center justify-center">
           <img src="/xelix_logo.svg" alt="Logo" className="w-8 h-8" />
         </Link>
       </div>
       <nav className="flex-1 flex flex-col items-center py-4 gap-6">
-        {/* Arrows icon */}
-        <Link href="#" className="flex h-10 w-10 items-center justify-center rounded-md bg-purple-700">
-          <FontAwesomeIcon icon={faArrowRightArrowLeft} className="h-4 w-4 text-white" />
+        {/* Invoice icon */}
+        <Link href="#" className="flex h-10 w-10 items-center justify-center rounded-md bg-purple-700" title="Invoice Processing">
+          <FontAwesomeIcon icon={faFileInvoice} className="h-4 w-4 text-white" />
         </Link>
         
         {/* Document icon */}
-        <Link href="#" className="flex h-10 w-10 items-center justify-center">
-          <FontAwesomeIcon icon={faFileLines} className="h-5 w-5 text-white" />
+        <Link href="#" className="flex h-10 w-10 items-center justify-center" title="Transactions">
+          <FontAwesomeIcon icon={faArrowRightArrowLeft} className="h-5 w-5 text-white" />
         </Link>
         
-        {/* ID card icon */}
-        <Link href="#" className="flex h-10 w-10 items-center justify-center">
-          <FontAwesomeIcon icon={faIdCard} className="h-5 w-5 text-white" />
+        {/* File check icon */}
+        <Link href="#" className="flex h-10 w-10 items-center justify-center" title="Statements">
+          <div className="relative">
+            <FontAwesomeIcon icon={faFile} className="h-5 w-5 text-white" />
+            <FontAwesomeIcon icon={faCheck} className="h-3 w-3 text-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[20%]" />
+          </div>
         </Link>
         
-        {/* Analytics/chart icon */}
-        <Link href="#" className="flex h-10 w-10 items-center justify-center">
-          <FontAwesomeIcon icon={faChartBar} className="h-5 w-5 text-white" />
+        {/* Address book icon */}
+        <Link href="#" className="flex h-10 w-10 items-center justify-center" title="Vendors">
+          <FontAwesomeIcon icon={faAddressBook} className="h-5 w-5 text-white" />
         </Link>
         
-        {/* Document with lines icon */}
-        <Link href="#" className="flex h-10 w-10 items-center justify-center">
-          <FontAwesomeIcon icon={faFileAlt} className="h-5 w-5 text-white" />
+        {/* Line chart icon */}
+        <Link href="#" className="flex h-10 w-10 items-center justify-center" title="Reports">
+          <FontAwesomeIcon icon={faChartLine} className="h-5 w-5 text-white" />
+        </Link>
+
+        {/* Inbox icon */}
+        <Link href="#" className="flex h-10 w-10 items-center justify-center" title="Helpdesk">
+          <FontAwesomeIcon icon={faInbox} className="h-5 w-5 text-white" />
         </Link>
         
         {/* Setting gear at the bottom */}
         <div className="mt-auto mb-6">
-          <Link href="#" className="flex h-10 w-10 items-center justify-center">
+          <Link href="#" className="flex h-10 w-10 items-center justify-center" title="Settings">
             <FontAwesomeIcon icon={faGear} className="h-5 w-5 text-white" />
           </Link>
         </div>
